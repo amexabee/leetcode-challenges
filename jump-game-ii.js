dba6781b98d0932f6bc133ff085e837d412df737
Amanuel Galema <amexabee@yahoo.com>
2023-04-26 20:04:50 +0300

    Update jump-game-ii.js

--- jump-game-ii.js
+++ jump-game-ii.js
@@ -1,4 +1,4 @@
-/*   Challenge description  (Difficulty: Medium)
+/*   Challenge description  https://leetcode.com/problems/jump-game-ii/
 
 You are given a 0-indexed array of integers nums of length n. You are initially positioned 
 at nums[0]. Each element nums[i] represents the maximum length of a forward jump from index i. 
