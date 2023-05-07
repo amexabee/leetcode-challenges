36cf797f85773fc7fc83046dcde965b31cc0bd44
Amanuel Galema <amexabee@yahoo.com>
2023-05-07 08:07:04 +0300

    Solve 3sum

--- 3sum.js
+++ 3sum.js
@@ -7,5 +7,27 @@ Notice that the solution set must not contain duplicate triplets.
 */
 
 var threeSum = function(nums) {
-    
+  let res = []
+  nums.sort((a, b) => a - b);
+  
+  for (let i = 0; i < nums.length; i++) {
+    if (i === 0 || nums[i] !== nums[i - 1]) {
+      let left = i + 1;
+      let right = nums.length - 1;
+      
+      while (left < right) {
+        const sum = nums[i] + nums[left] + nums[right];
+        if (sum === 0) {
+          res.push([nums[i], nums[left], nums[right]])
+          while (nums[left] === nums[left + 1]) left++;
+          while (nums[right] === nums[right - 1]) right--;
+          left++;
+          right++;
+        } else if (sum < 0) left++;
+        else right--;
+      }
+    }
+  }
+  
+  return res;
 };
