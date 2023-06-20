026ec77ba67db25d40a441f0be28e780f6548410
Amanuel Galema <amexabee@yahoo.com>
2023-06-20 14:36:59 +0300

    Solve longest-palindromic-substring.js

--- longest-palindromic-substring.js
+++ longest-palindromic-substring.js
@@ -17,5 +17,23 @@ s consist of only digits and English letters.
 */
 
 var longestPalindrome = function(s) {
-    
+  let longest = s[0] || '';
+
+  for (let i = 0; i < s.length; i++) {
+    for (let j = i + 1; j < s.length; j++) {
+      longest = isIt(i, j) && (j - i + 1) > longest.length ? s.substring(i, j+1) : longest;
+    }
+  }
+  
+  function isIt(p1, p2) {
+
+    while (p1 < p2) {
+      if (s[p1] !== s[p2]) return false;
+      p1++;
+      p2--;
+    } 
+    return true
+  }
+
+  return longest;    
 };
