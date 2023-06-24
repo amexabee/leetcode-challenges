aef1e1fad744a33942e2444a244f33ec9a495797
Amanuel Galema <amexabee@yahoo.com>
2023-06-24 17:32:39 +0300

    Update problem description

--- reverse-integer.js
+++ reverse-integer.js
@@ -1,7 +1,7 @@
 /* 
 Challenge description: https://leetcode.com/problems/reverse-integer/
 
-Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
+Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-2^31, 2^31 - 1], then return 0.
 
 Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
 
@@ -18,7 +18,7 @@ Input: x = 120
 Output: 21
  
 Constraints:
--231 <= x <= 231 - 1
+-2^31 <= x <= 2^31 - 1
 */
 
 var reverse = function(x) {
