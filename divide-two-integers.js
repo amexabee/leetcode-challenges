c0b413fa529958c76cbf127ea1b40227ec95819c
Amanuel Galema <amexabee@yahoo.com>
2023-07-09 19:22:00 +0300

    Solve challenge

--- divide-two-integers.js
+++ divide-two-integers.js
@@ -5,7 +5,7 @@ Given two integers dividend and divisor, divide two integers
 without using multiplication, division, and mod operator.
 The integer division should truncate toward zero, which means losing its fractional part. 
 For example, 8.345 would be truncated to 8, and -2.7335 would be truncated to -2.
-Return the quotient after dividing dividend by divisor.
+Return the quotient after dividing the dividend by the divisor.
 
 Note: Assume we are dealing with an environment that could only store integers 
 within the 32-bit signed integer range: [−231, 231 − 1]. For this problem, 
@@ -28,5 +28,27 @@ divisor != 0
 */
 
 var divide = function(dividend, divisor) {
+    let quotient = 0
+    let negative = (dividend < 0 && divisor > 0) !== (dividend > 0 && divisor < 0);
 
-}
+    dividend = Math.abs(dividend)
+    divisor = Math.abs(divisor)
+
+    while(dividend >= divisor) {
+        let multiple = 1;
+        let temp = divisor;
+
+        while (dividend >= temp) {
+            dividend -= temp;
+            temp += temp;
+            quotient += multiple;
+            multiple += multiple;
+        }
+    }
+
+    if (negative) quotient = -quotient;
+    
+    const minInt = Math.pow(-2, 31);
+  
+    return Math.max(Math.min(quotient, -minInt - 1), minInt); 
+};
