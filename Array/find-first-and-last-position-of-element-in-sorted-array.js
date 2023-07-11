13d0fa76a81f25a7acdcd2ec44252028c778c307
Amanuel Galema <amexabee@yahoo.com>
2023-07-11 19:00:14 +0300

    Solve challenge without JS array methods

--- Array/find-first-and-last-position-of-element-in-sorted-array.js
+++ Array/find-first-and-last-position-of-element-in-sorted-array.js
@@ -25,5 +25,56 @@ nums is a non-decreasing array.
 */
 
 var searchRange = function(nums, target) {
- return [nums.indexOf(target), nums.lastIndexOf(target)];
+ // return [nums.indexOf(target), nums.lastIndexOf(target)];
+  const firstIndex = (nums, target) => { 
+    let first = -1;
+    
+    recursive(0, nums.length - 1)
+    
+    function recursive(start, end) {
+      if (start > end) return;
+      
+      if (start === end - 1 || start === end) {
+        if (nums[start] === target) first = start;
+        else if (nums[end] === target) first = end;
+        return;
+      }
+      
+      let middle = Math.floor((start + end) / 2);
+      
+      if (nums[middle] >= target) {
+        if (nums[middle] === target) first = middle;
+        recursive(start, middle);
+      } else recursive(middle, end);
+    }
+
+    return first;
+  }
+
+  const lastIndex = (nums, target) => { 
+    let last = -1;
+    
+    recursive(0, nums.length - 1)
+    
+    function recursive(start, end) {
+      if (start > end) return;     
+      
+      if (start === end - 1 || start === end) {
+        if (nums[end] === target) last = end;
+        else if (nums[start] === target) last = start;
+        return;
+      }
+      
+      let middle = Math.floor((start + end) / 2);
+      
+      if (nums[middle] <= target) {
+        if (nums[middle] === target) last = middle;
+        recursive(middle, end);
+      } else recursive(start, middle);
+    }
+
+    return last;
+  }
+
+  return [firstIndex(nums, target), lastIndex(nums, target)];
 }
