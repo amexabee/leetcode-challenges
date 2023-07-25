9347056a34de86d6a49d034aec543b1623060bba
Amanuel Galema <amexabee@yahoo.com>
2023-07-26 00:29:08 +0300

    Solve insert-interval.js

--- Array/insert-interval.js
+++ Array/insert-interval.js
@@ -26,5 +26,44 @@ newInterval.length == 2
 */
 
 var insert = function(intervals, newInterval) {
+  if (intervals.length === 0) {
+    intervals.push(newInterval)
+    return intervals;
+  } 
   
+  recursive(0, intervals.length - 1)
+  
+  function recursive(start, end) {
+    let i = Math.floor((start + end) / 2)
+    if (newInterval[0] > intervals[i][1]) {
+      if (start === end) {
+        intervals.splice(i + 1, 0, newInterval);
+        return;
+      }
+      recursive(i + 1, end);
+    }
+  
+    else if (newInterval[1] < intervals[i][0]){
+      if (start === end) {
+        intervals.splice(i, 0, newInterval);
+        return;
+      }
+      recursive(start, i - 1);
+    }
+
+    else {
+      let c = i;
+      while(intervals[c] && newInterval[1] >= intervals[c][0]) c++;
+      intervals[i][1] = Math.max(newInterval[1], intervals[c - 1][1])
+      intervals.splice(i + 1, c - i - 1);
+  
+      
+      c = i - 1;
+      while(intervals[c] && newInterval[0] <= intervals[c][1]) c--;
+      intervals[i][0] = Math.min(newInterval[0], intervals[c + 1][0]);
+      intervals.splice(c + 1, i - c - 1)
+    }
+  }
+
+  return intervals;
 }
