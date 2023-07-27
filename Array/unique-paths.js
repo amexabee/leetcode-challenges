b00475277513aecce31f6124bd151059dfcca416
Amanuel Galema <amexabee@yahoo.com>
2023-07-27 23:38:08 +0300

    Solve unique-paths.js

--- Array/unique-paths.js
+++ Array/unique-paths.js
@@ -25,4 +25,16 @@ Constraints:
 
 var uniquePaths = function(m, n) {
 
+  function factorial(n) {
+    if (n === 0 || n === 1) {
+      return 1;
+    }
+
+    let result = 1;
+    for (let i = 2; i <= n; i++) {
+      result *= i;
+    }
+    return result;
+  }
+  return  factorial(m + n - 2)/(factorial(m - 1) * factorial(n - 1))
 }
