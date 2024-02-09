8a5398268c8440d17528cddfb53ea4e560a9e2f7
Amanuel Galema <amexabee@yahoo.com>
2024-02-09 20:26:12 +0200

    Create bubblesort.js

---
+++ personal/bubblesort.js
@@ -0,0 +1,13 @@
+const bubble = arr => {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - 1; j++) {
+      if (arr[j] > arr[j+1]) {
+        let temp = arr[j];
+        arr[j] = arr[j+1];
+        arr[j+1] = temp;
+      }
+    }
+  }
+
+  return arr;
+}
