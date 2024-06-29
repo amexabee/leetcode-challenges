7640bc88d3407c7a3ed9804cdaacb100019daed3
Amanuel Galema <amexabee@yahoo.com>
2024-06-29 21:59:32 +0300

    Solve restore-ip-addresses.js

--- String/restore-ip-addresses.js
+++ String/restore-ip-addresses.js
@@ -23,6 +23,27 @@ Input: s = "101023"
 Output: ["1.0.10.23","1.0.102.3","10.1.0.23","10.10.2.3","101.0.2.3"]
 */
 
-var restoreIpAddresses = function(s) {
+var restoreIpAddresses = function(s, group = 4, pre = "", ip = "", result = []) {
+  if (s.length < 1) return result;
 
+  if (group === 1 && s.length <= 3 && (s.length === 1 || s[0] !== "0") && parseInt(s) <= 255) {
+    ip += pre + "." + s;
+    result.push(ip);
+    return result;
+  }
+
+  max = group * 3;
+  if (s.length > max) return result;
+
+  if (pre) ip += pre + ".";
+
+  for (let i = 1; i <= 3; i++) {
+    pre = s.slice(0, i);
+    if (pre.length === 3 && parseInt(pre) > 255) continue;
+    if (pre.length > 1 && pre[0] === "0") continue;
+    let last = s.slice(i);
+    restoreIpAddresses(last, group - 1, pre, ip, result);
+  }
+
+  return result;
 }
