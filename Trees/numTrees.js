23bdab3b7afd1e3fbfe1ffddb5d9b889aa64a8cb
Amanuel Galema <amexabee@yahoo.com>
2025-01-07 18:02:57 +0200

    Try a pseudocode

--- Trees/numTrees.js
+++ Trees/numTrees.js
@@ -14,7 +14,26 @@ Output: 1
 
 Constraints:
 1 <= n <= 19
+
+PSEUDOCODE (RECURSIVE APPROACH)
+n = 3
+    3   
+  2
+1
+
+1
+
+1          2
+  2      1
+
+1          3          3       1         2
+  2     1           2            3    1   3
+    3     2       1            2
+
+
+
 */
+
 
 var numTrees = function(n) {
     
